fee1763d2d6d04ddd3b8a70053a58753da74e1f9
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-08-29 10:31:05 -0500

    Adding Week-4 - Part II

---
+++ week-4/reactive-composer-app/src/app/about/about.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: about.composer.ts
+ * Author: Travis Rosen
+ * Date: 22 August 2021
+ * Description: About Details Component (About specific Composer)
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
