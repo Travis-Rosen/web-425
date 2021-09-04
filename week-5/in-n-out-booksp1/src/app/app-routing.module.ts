a9bd2c33cf4d39d395545f4c76746eb6660236e3
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-09-04 13:03:41 -0500

    Adding Week-5 Part I

---
+++ week-5/in-n-out-booksp1/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+// Author: Travis Rosen
+// Date: 09/04/2021
+// Name: app.routing-module.ts
+// Description: Routing page
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListComponent,
+  },
+  {
+    path: 'book-list',
+    component: BookListComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
