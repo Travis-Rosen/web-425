95b2152f3b01cdaa176d854a1edba90e08cd1833
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-09-12 19:27:26 -0500

    Adding Week-6 Part III

---
+++ week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
@@ -0,0 +1,22 @@
+// Author: Travis Rosen
+// Date: 09/12/2021
+// Name: gpa.component.ts
+// Description: Variables for gpa.component
+
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gpa',
+  templateUrl: './gpa.component.html',
+  styleUrls: ['./gpa.component.css']
+})
+export class GpaComponent implements OnInit {
+
+  @Input() gpaTotal: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
